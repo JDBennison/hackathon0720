28d43eceddeb8dbc5c00dd2bdc554b1cc31cf946
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-07 13:53:42 +0100

    javascript edits, css edits, modal button templates, hover lick pop over stying

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,9 +4,12 @@
 //     });
 // });
 
-// Modal brainteasers button colours //
+// - - - - - - - - - - -  - - - - - - - - - - -  Modal brainteasers button popovers //
 
 $(function () {
     $('[data-toggle="popover"]').popover()
   })
 
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  })
