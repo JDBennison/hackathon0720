6449026827707041b2cebf09aa4c68a0bd5e43c3
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-07 13:10:45 +0100

    js tests and linking tests

---
+++ javascript/javascript.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     $('#answers').click(function() {
+//         $('.answer').fadeToggle();
+//     });
+// });
+
+// Modal brainteasers button colours //
+
+// $(function () {
+//     $('[data-toggle="popover"]').popover()
+//   })
+
+$(document).ready(function () {
+    console.log("LETS GO")
+});
+
+$('#testjava').click(function () { 
+    alert("reached") 
+    console.log("IT IS LINKED")
+});
--- scripts/js/answer.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('#answers').click(function() {
-        $('.answer').fadeToggle();
-    });
-});
