3d8a2797023f7b199a4b6889084e6d23436d794c
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-07 13:28:15 +0100

    js test remove

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,15 +6,7 @@
 
 // Modal brainteasers button colours //
 
-// $(function () {
-//     $('[data-toggle="popover"]').popover()
-//   })
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
 
-// $(document).ready(function () {
-//     console.log("LETS GO")
-// });
-
-$('#testjava').click(function () { 
-    alert("reached") 
-    console.log("IT IS LINKED")
-});
